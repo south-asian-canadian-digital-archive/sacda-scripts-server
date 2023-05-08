758f197290cd3b8ec0968c4bc569a293fe86849f
devnull03 <56480041+devnull03@users.noreply.github.com>
2023-05-08 14:22:58 -0700

    fix: build edit

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,14 +1,13 @@
-import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+// import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter({
-			fallback: '200.html'
-		})
+		adapter: adapter()
 	}
 };
 
